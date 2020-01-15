fa6a95d0c94389324fb8662e21f679232a600648
Lucas Lima <50705998+lucas85235@users.noreply.github.com>
2020-01-15 08:53:40 -0400

    Refact Miller Code (WIP)

---
+++ CuberZ/Assets/-Game/Scripts/Camera/CameraController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    [SerializeField] private float angleMin = -80.0f;
+    [SerializeField] private float angleMax = -1.0f;
+
+
+    private RaycastHit hit_ = new RaycastHit();
+    private LayerMask layer_;
+    private Transform target_;
+
+    private float currentX = 0.0f;
+    private float currentY = 0.0f;
+
+    public float mouseX = 10.0f;
+    public float mouseY = 10.0f;
+    public float zoonInPosition = 8.0f;
+    public float cameraPosition = -18.0f;
+
+    void Start()
+    {
+        target_ = GameObject.Find("LOOK").transform;
+        layer_ = LayerMask.GetMask("Default");
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            currentX += Input.GetAxis("Horizontal") * mouseX + Input.GetAxis("Mouse X") * mouseX;
+            currentY -= Input.GetAxis("Vertical") * mouseY + Input.GetAxis("Mouse Y") * mouseY;
+
+            currentY = Mathf.Clamp(currentY, angleMin, angleMax);
+            Vector3 dir = new Vector3(0, 0, zoonInPosition);
+            Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+            transform.position = target_.position + rotation * dir;
+            transform.LookAt(target_.position);
+
+        }
+
+        transform.position = target_.position - transform.forward * zoonInPosition;
+
+        if (Physics.Linecast(target_.position, transform.position, out hit_, layer_))
+        {
+            transform.position = hit_.point + transform.forward * cameraPosition;
+        }
+    }
+}
---
+++ CuberZ/Assets/-Game/Scripts/Monster/Moster.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moster : MonoBehaviour
+{
+    private Rigidbody boby_;
+    private Animator animator_;
+    private GameObject collider_;
+    private GameObject camera_;
+
+    private float smooth_;
+
+    public float speed = 15.0f;
+    public float smoothTime = 0.3f;
+    public float attackSpeed = 30.0f;
+    public float attackDistance = 5.0f;
+
+    void Start()
+    {
+        boby_ = GetComponent<Rigidbody>();
+        animator_ = GetComponent<Animator>();
+        camera_ = Camera.main.gameObject;
+
+        // mudar
+        // collider_ = transform.Find("COLISOR").gameObject;
+
+        boby_.freezeRotation = true;
+    }
+
+    void Update()
+    {
+        var currentAnimation = animator_.GetCurrentAnimatorStateInfo(0);
+
+        float X = Input.GetAxis("Horizontal");
+        float Z = Input.GetAxis("Vertical");
+
+        if (!PlayAttackAnimation())
+        {
+            Walk(X, Z);
+            AnimationSpeed(X, Z);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+            AttackDirection();
+
+        // collider_.SetActive(currentAnimation.IsName("Attack"));
+
+        if (currentAnimation.IsName("Attack"))
+        {
+            boby_.AddForce(transform.forward * attackSpeed);
+        }
+        else
+        {
+            if (ExistGround())
+                boby_.velocity = Vector3.zero;       
+        }
+    }
+
+    bool ExistGround()
+    {
+        return Physics.Raycast(transform.position, (-1 * transform.up), 1f);
+    }
+
+    private bool PlayAttackAnimation()
+    {
+        var animation = animator_.GetCurrentAnimatorStateInfo(0);
+        return animation.IsName("Attack");
+    }
+
+    void AttackDirection()
+    {
+        Ray ray = camera_.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (Vector3.Distance(transform.position, hit.point) > attackDistance)
+            {
+                transform.LookAt(hit.point);
+                animator_.SetTrigger("ATTACK");
+            }
+        }
+    }
+    void Walk(float X, float Z)
+    {
+        Vector2 input = new Vector2(X, Z);
+
+        if (input != Vector2.zero)
+        {
+            float targetrotation = Mathf.Atan2(input.x, input.y) * Mathf.Rad2Deg;
+            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(
+                transform.eulerAngles.y, 
+                targetrotation + camera_.transform.eulerAngles.y, 
+                ref smooth_, 
+                smoothTime);
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+    }
+
+    void AnimationSpeed(float axisX, float axisY)
+    {
+        animator_.SetFloat("SPEED", axisX * axisX + axisY * axisY);
+    }
+}
