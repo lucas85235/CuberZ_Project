39122b0034e8a6a178b78be606aabf9f58bba189
Lucas Lima <lucas85235@gmail.com>
2020-01-23 16:35:40 -0400

    Fix codeStyle

--- CuberZ/Assets/-Game/Scripts/InputManager/InputSystem.cs
+++ CuberZ/Assets/-Game/Scripts/InputManager/InputSystem.cs
@@ -4,107 +4,99 @@ using UnityEngine;
 
 public class InputSystem : MonoBehaviour, IInput
 {
-
-    static InputSystem instance_;
-    public static InputSystem Instance_ { get { return instance_; } }
+    private static InputSystem instance_;
+    public static InputSystem instance { get { return instance_; } }
 
    
     [Header("Teclas Configuráveis")]
-    public KeyCode moveCameraKey_ = KeyCode.Mouse1;
-    public KeyCode goToBattleKey_ = KeyCode.Mouse0;
-    public KeyCode kubberAtk1Key_ = KeyCode.Alpha1;
-    public KeyCode kubberAtk2Key_ = KeyCode.Alpha2;
-    public KeyCode kubberAtk3Key_ = KeyCode.Alpha3;
-    public KeyCode kubberAtk4Key_ = KeyCode.Alpha4;
-    public string getAxisHorizontal_ = "Horizontal";
-    public string getAxisVertical_ = "Vertical";
-    public KeyCode fixCameraOnMyKubberKey_ = KeyCode.Tab;
-    public KeyCode rescueKubberKey_ = KeyCode.LeftControl;
-    public KeyCode runKey_ = KeyCode.LeftShift;
-    public KeyCode exitKey_ = KeyCode.Escape;
-    public KeyCode captureKubberkey_ = KeyCode.Backspace;
+    public KeyCode moveCameraKey = KeyCode.Mouse1;
+    public KeyCode goToBattleKey = KeyCode.Mouse0;
+    public KeyCode kubberAtk1Key = KeyCode.Alpha1;
+    public KeyCode kubberAtk2Key = KeyCode.Alpha2;
+    public KeyCode kubberAtk3Key = KeyCode.Alpha3;
+    public KeyCode kubberAtk4Key = KeyCode.Alpha4;
+    public string getAxisHorizontal = "Horizontal";
+    public string getAxisVertical = "Vertical";
+    public KeyCode fixCameraOnMyKubberKey = KeyCode.Tab;
+    public KeyCode rescueKubberKey = KeyCode.LeftControl;
+    public KeyCode runKey = KeyCode.LeftShift;
+    public KeyCode exitKey = KeyCode.Escape;
+    public KeyCode captureKubberkey = KeyCode.Backspace;
 
     private void Awake()
     {
-        instance_ = this;
+        instance = this;
     }
 
     #region Variáveis PlayerPrefs
-    private const string moveCameraPlayerPref_ = "moveCameraPlayerPref_";
-    private const string goToBattlePlayerPref_ = "goToBattlePlayerPref_";
-    private const string kubberAtk1PlayerPref_ = "kubberAtk1PlayerPref_";
-    private const string kubberAtk2PlayerPref_ = "kubberAtk2PlayerPref_";
-    private const string kubberAtk3PlayerPref_ = "kubberAtk3PlayerPref_";
-    private const string kubberAtk4PlayerPref_ = "kubberAtk4PlayerPref_";
-    private const string getAxisHorizontalPlayerPref_ = "getAxisHorizontalPlayerPref_";
-    private const string getAxisVerticalPlayerPref_ = "getAxisVerticalPlayerPref_";
-    private const string fixCameraOnMyKubberPlayerPref_ = "fixCameraOnMyKubberPlayerPref_";
-    private const string rescueKubberPlayerPref_ = "rescueKubberPlayerPref_";
-    private const string runPlayerPref_ = "runPlayerPref_";
-    private const string exitPlayerPref_ = "exitPlayerPref_";
-    private const string captureKubberPref_ = "captureKubberPref_";
-
+    private const string moveCameraPlayerPref = "MOVE-CAMERA";
+    private const string goToBattlePlayerPref = "GO-TO-BATTLE";
+    private const string kubberAtk1PlayerPref = "KUBBER-ATTTACK-1";
+    private const string kubberAtk2PlayerPref = "KUBBER-ATTTACK-2";
+    private const string kubberAtk3PlayerPref = "KUBBER-ATTTACK-3";
+    private const string kubberAtk4PlayerPref = "KUBBER-ATTTACK-4";
+    private const string getAxisHorizontalPlayerPref = "GET-AXIS-HORIZONTAL";
+    private const string getAxisVerticalPlayerPref = "GET-AXIS-VERTICAL";
+    private const string fixCameraOnMyKubberPlayerPref = "FIX-CAMERA-ON-MY-KUBBER";
+    private const string rescueKubberPlayerPref = "RESCUE-KUBBER";
+    private const string runPlayerPref = "RUN-PLAYER";
+    private const string exitPlayerPref = "EXIT-PLAYER";
+    private const string captureKubberPref = "CAPTURE-KUBBER";
     #endregion
 
     #region Funções Singleton
-
-    public bool MoveCameraInput() { return Input.GetKey(moveCameraKey_); }
-    public bool GoToBattleInput() { return Input.GetKeyDown(goToBattleKey_); }
-    public bool KubberAtk1Input() { return Input.GetKeyDown(kubberAtk1Key_); }
-    public bool KubberAtk2Input() { return Input.GetKeyDown(kubberAtk2Key_); }
-    public bool KubberAtk3Input() { return Input.GetKeyDown(kubberAtk3Key_); }
-    public bool KubberAtk4Input() { return Input.GetKeyDown(kubberAtk4Key_); }
-    public float GetAxisHorizontal() { return Input.GetAxis(getAxisHorizontal_); }
-    public float GetAxisVertical() { return Input.GetAxis(getAxisVertical_); }
-    public bool FixCameraOnMyKubberInput() { return Input.GetKeyDown(fixCameraOnMyKubberKey_); }
-    public bool RescueKubberInput() { return Input.GetKeyDown(rescueKubberKey_); }
-    public bool RunInput() { return Input.GetKey(runKey_); }
-    public bool ExitInput() { return Input.GetKeyDown(exitKey_); }
-    public bool CaptureKubberInput() { return Input.GetKeyDown(captureKubberkey_); }
-
-
+    public bool MoveCameraInput() { return Input.GetKey(moveCameraKey); }
+    public bool GoToBattleInput() { return Input.GetKeyDown(goToBattleKey); }
+    public bool KubberAtk1Input() { return Input.GetKeyDown(kubberAtk1Key); }
+    public bool KubberAtk2Input() { return Input.GetKeyDown(kubberAtk2Key); }
+    public bool KubberAtk3Input() { return Input.GetKeyDown(kubberAtk3Key); }
+    public bool KubberAtk4Input() { return Input.GetKeyDown(kubberAtk4Key); }
+    public float GetAxisHorizontal() { return Input.GetAxis(getAxisHorizontal); }
+    public float GetAxisVertical() { return Input.GetAxis(getAxisVertical); }
+    public bool FixCameraOnMyKubberInput() { return Input.GetKeyDown(fixCameraOnMyKubberKey); }
+    public bool RescueKubberInput() { return Input.GetKeyDown(rescueKubberKey); }
+    public bool RunInput() { return Input.GetKey(runKey); }
+    public bool ExitInput() { return Input.GetKeyDown(exitKey); }
+    public bool CaptureKubberInput() { return Input.GetKeyDown(captureKubberkey); }
     #endregion
 
     #region Funções PlayerPrefs
-
-    public void SaveAllKeyCodes() //Salva todas as Keys
+    public void SaveAllKeyCodes()
     {
+        PlayerPrefs.SetString(moveCameraPlayerPref, moveCameraKey.ToString());
+        PlayerPrefs.SetString(goToBattlePlayerPref, goToBattleKey.ToString());
+        PlayerPrefs.SetString(kubberAtk1PlayerPref, kubberAtk1Key.ToString());
+        PlayerPrefs.SetString(kubberAtk2PlayerPref, kubberAtk2Key.ToString());
+        PlayerPrefs.SetString(kubberAtk3PlayerPref, kubberAtk3Key.ToString());
+        PlayerPrefs.SetString(kubberAtk4PlayerPref, kubberAtk4Key.ToString());
+        PlayerPrefs.SetString(getAxisHorizontalPlayerPref, getAxisHorizontal);
+        PlayerPrefs.SetString(getAxisVerticalPlayerPref, getAxisVertical);
+        PlayerPrefs.SetString(fixCameraOnMyKubberPlayerPref, fixCameraOnMyKubberKey.ToString());
+        PlayerPrefs.SetString(rescueKubberPlayerPref, rescueKubberKey.ToString());
+        PlayerPrefs.SetString(runPlayerPref, runKey.ToString());
+        PlayerPrefs.SetString(exitPlayerPref, exitKey.ToString());
+        PlayerPrefs.SetString(captureKubberPref, captureKubberkey.ToString());
 
-        PlayerPrefs.SetString(moveCameraPlayerPref_, moveCameraKey_.ToString());
-        PlayerPrefs.SetString(goToBattlePlayerPref_, goToBattleKey_.ToString());
-        PlayerPrefs.SetString(kubberAtk1PlayerPref_, kubberAtk1Key_.ToString());
-        PlayerPrefs.SetString(kubberAtk2PlayerPref_, kubberAtk2Key_.ToString());
-        PlayerPrefs.SetString(kubberAtk3PlayerPref_, kubberAtk3Key_.ToString());
-        PlayerPrefs.SetString(kubberAtk4PlayerPref_, kubberAtk4Key_.ToString());
-        PlayerPrefs.SetString(getAxisHorizontalPlayerPref_, getAxisHorizontal_);
-        PlayerPrefs.SetString(getAxisVerticalPlayerPref_, getAxisVertical_);
-        PlayerPrefs.SetString(fixCameraOnMyKubberPlayerPref_, fixCameraOnMyKubberKey_.ToString());
-        PlayerPrefs.SetString(rescueKubberPlayerPref_, rescueKubberKey_.ToString());
-        PlayerPrefs.SetString(runPlayerPref_, runKey_.ToString());
-        PlayerPrefs.SetString(exitPlayerPref_, exitKey_.ToString());
-        PlayerPrefs.SetString(captureKubberPref_, captureKubberkey_.ToString());
         Debug.Log("Todas as Keys Foram Salvas");
     }
 
-    public void LoadAllKeyCodes() //Da load em todas as Keys
+    public void LoadAllKeyCodes()
     {
-        moveCameraKey_ = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString(moveCameraPlayerPref_));
-        goToBattleKey_ = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString(goToBattlePlayerPref_));
-        kubberAtk1Key_ = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString(kubberAtk1PlayerPref_));
-        kubberAtk2Key_ = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString(kubberAtk2PlayerPref_));
-        kubberAtk3Key_ = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString(kubberAtk3PlayerPref_));
-        kubberAtk4Key_ = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString(kubberAtk4PlayerPref_));
-        fixCameraOnMyKubberKey_ = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString(fixCameraOnMyKubberPlayerPref_));
-        rescueKubberKey_ = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString(rescueKubberPlayerPref_));
-        runKey_ = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString(runPlayerPref_));
-        exitKey_ = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString(exitPlayerPref_));
-        getAxisHorizontal_ = PlayerPrefs.GetString(getAxisHorizontalPlayerPref_);
-        getAxisVertical_ = PlayerPrefs.GetString(getAxisVerticalPlayerPref_);
-        captureKubberkey_ = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString(captureKubberPref_));
+        moveCameraKey = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString(moveCameraPlayerPref));
+        goToBattleKey = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString(goToBattlePlayerPref));
+        kubberAtk1Key = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString(kubberAtk1PlayerPref));
+        kubberAtk2Key = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString(kubberAtk2PlayerPref));
+        kubberAtk3Key = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString(kubberAtk3PlayerPref));
+        kubberAtk4Key = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString(kubberAtk4PlayerPref));
+        fixCameraOnMyKubberKey = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString(fixCameraOnMyKubberPlayerPref));
+        rescueKubberKey = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString(rescueKubberPlayerPref));
+        runKey = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString(runPlayerPref));
+        exitKey = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString(exitPlayerPref));
+        getAxisHorizontal = PlayerPrefs.GetString(getAxisHorizontalPlayerPref);
+        getAxisVertical = PlayerPrefs.GetString(getAxisVerticalPlayerPref);
+        captureKubberkey = (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString(captureKubberPref));
+        
         Debug.Log("Todas as Keys Foram Carregadas");
     }
-
-
-
     #endregion
 }
