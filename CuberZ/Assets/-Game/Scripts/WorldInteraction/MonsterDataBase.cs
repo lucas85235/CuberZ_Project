fae86c3c3e8c991740167dcc6458c6e9768f193d
Matheus Cardoso <cardoso33333@gmail.com>
2020-02-06 14:46:47 -0300

    Adicionado MonsterDataBase
    -Ao clicar me um monstro, verifica se ele ja foi visto alguma vez ao comprar com o data base.
    
    -Esse script não tem save automático por enquanto.

---
+++ CuberZ/Assets/-Game/Scripts/WorldInteraction/MonsterDataBase.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterDataBase : MonoBehaviour
+{
+
+    public MonsterIndividual[] monster;
+
+    #region Singleton
+    public static MonsterDataBase instance { get { return instance_; } }
+    private static MonsterDataBase instance_;
+
+    private void Awake()
+    {
+        instance_ = this;
+    }
+
+    #endregion
+}
+
+[System.Serializable]
+public class MonsterIndividual
+{
+    public string monsterName;
+    public MonsterBase monster;
+    public bool beenSeen;
+    //Opção de colocar todos os stats aqui talvez? 
+}
