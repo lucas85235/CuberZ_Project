75a072511b9e1c3bf72d46326f1e023e38f1e403
Matheus Cardoso <cardoso33333@gmail.com>
2020-02-12 09:47:16 -0300

    BugFix EyeAnimation script.

--- CuberZ/Assets/-Game/Scripts/Character/Animation/EyeAnimation.cs
+++ CuberZ/Assets/-Game/Scripts/Character/Animation/EyeAnimation.cs
@@ -2,60 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EyeAnimation : MonoBehaviour 
+public class EyeAnimation : MonoBehaviour
 {
-	private SkinnedMeshRenderer skinnedMesh_;
+    private SkinnedMeshRenderer skinnedMesh_;
 
-	private int index_;
-	private float countTime_;
+    private int index_;
+    private float countTime_;
     private float secondCountTime_;
     private float waitTime_;
     private float morph_;
 
 
-	void Start () 
+    void Start()
     {
-		skinnedMesh_ = GetComponent<SkinnedMeshRenderer> ();
-		morph_ = 0;
-	}
+        skinnedMesh_ = GetComponent<SkinnedMeshRenderer>();
+        morph_ = 0;
+    }
 
-	void Update () 
+    void Update()
     {
-		skinnedMesh_.SetBlendShapeWeight (0, Mathf.Lerp (skinnedMesh_.GetBlendShapeWeight (0), morph_, 0.4f) );
+        skinnedMesh_.SetBlendShapeWeight(0, Mathf.Lerp(skinnedMesh_.GetBlendShapeWeight(0), morph_, 20 * Time.deltaTime));
 
-		if (index_ < 2) 
+        if (index_ < 2)
         {
-			if (countTime_ < waitTime_) 
+            if (countTime_ < waitTime_)
             {
-				countTime_ += 1f;
-			} 
-            else 
+                countTime_ += 1f;
+            }
+            else
             {
-				if (morph_ <=1) 
+                if (morph_ <= 1)
                 {
-					morph_ = 100;
-					waitTime_ = 3;
-				} 
-                else 
+                    morph_ = 100;
+                    waitTime_ = 3;
+                }
+                else
                 {
-					morph_ = 0;
-					waitTime_ = 50;
-				}
-				index_++;
-				countTime_ = 0;
-			}
-		}
-		if (index_ >= 2) 
+                    morph_ = 0;
+                    waitTime_ = 50;
+                }
+                index_++;
+                countTime_ = 0;
+            }
+        }
+        if (index_ >= 2)
         {
-			if (secondCountTime_ < waitTime_ * 2) 
+            if (secondCountTime_ < waitTime_ * 2)
             {
-				secondCountTime_ += 1;
-			} 
-            else 
+                secondCountTime_ += 1;
+            }
+            else
             {
-				index_ = 0;
-				secondCountTime_ = 0;
-			}
-		}
-	}
+                index_ = 0;
+                secondCountTime_ = 0;
+            }
+        }
+    }
 }
