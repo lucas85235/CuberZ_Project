828cf7194aebe5591279cc025e3f9a0af9b2b1fd
Alandrei-Santos <alandrei93@gmail.com>
2020-03-07 12:32:24 -0400

    Add database system

---
+++ CuberZ/Assets/-Game/Scripts/Database/API/IDataBase.cs
@@ -0,0 +1,13 @@
+﻿/******************************************************************************
+* Copyright (c) 2020 Herolix Studio
+* All rights reserved.
+******************************************************************************/
+
+public interface IDatabase
+{
+    void CreateDatabase(string dbName);
+    void OpenConnection(string dbName);
+    void CloseConnection();
+    void CreateTable(string tableName, string sqlQuery);
+    void ShowData(string tableName);
+}
